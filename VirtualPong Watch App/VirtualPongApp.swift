2472ba4e1346ab6a74c3858ff8ae89cdda1cb806
Crescenzo Esposito <crescenzo.esposito@outlook.it>
2023-03-03 21:22:10 +0100

    Initial commit

---
+++ VirtualPong Watch App/VirtualPongApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualPongApp.swift
+//  VirtualPong Watch App
+//
+//  Created by Crescenzo Esposito on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct VirtualPong_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VirtualPong/VirtualPongApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualPongApp.swift
+//  VirtualPong
+//
+//  Created by Crescenzo Esposito on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct VirtualPongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
